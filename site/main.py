8a54aaace8e8f15238773b28581e76be5880d0e9
Paimoe <me@paimoe.com>
2011-10-27 12:57:55 +0800

    requests and fabric to reqs, update main.py

--- site/main.py
+++ site/main.py
@@ -4,6 +4,8 @@ from flask import render_template, url_for, request
 
 import feedparser
 import requests
+import json
+import sys,os
 
 app = Flask(__name__)
 
@@ -23,7 +25,29 @@ def fetch():
     if r.status_code != 200:
         return 'not 200 whattttt: %s' % r.status_code
         
+    j = json.loads(r.content)
+    
+    # Fuck it just loop it, its like 5 things
+    wanted = None
+    for result in j['results']:
+        if result['types'][0] == 'postal_code':
+            wanted = result
+            
+    if wanted is None:
+        return "FUCSLDjgB"
+        
+    # result hasss.. the post code
+    postcode = wanted['address_components'][0]['long_name']
+    #print wanted['address_components']
+    #print "POSTCODE IS " + postcode
+        
     return r.content
 
+
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    if 'local' in sys.argv:
+        app.run(debug=True)
+    else:
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
